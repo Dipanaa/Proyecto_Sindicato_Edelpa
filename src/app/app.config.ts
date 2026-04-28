da25a968d4080d9d17560240476a913c9ab53628
dotDipana <dguerrajoselin@gmail.com>
2026-04-28 12:48:50 -0400

    feat: implementacion de appcheck mediante recaptcha

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,8 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import { provideFirebaseApp, initializeApp, getApp } from '@angular/fire/app';
+import { provideAppCheck, ReCaptchaEnterpriseProvider, initializeAppCheck } from '@angular/fire/app-check';
 import { getFirestore, initializeFirestore, persistentLocalCache, persistentMultipleTabManager, provideFirestore } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
 
@@ -15,6 +16,13 @@ export const appConfig: ApplicationConfig = {
       return initializeFirestore(getApp(), {
         localCache: persistentLocalCache({ tabManager: persistentMultipleTabManager() })
       })
+    }),
+    provideAppCheck(() => {
+      if (isDevMode()) {
+        (self as any).FIREBASE_APPCHECK_DEBUG_TOKEN = true;
+      }
+      const provider = new ReCaptchaEnterpriseProvider('6LeqsMssAAAAAIZTp7zGqY0aIKpj7J6PVE0icrL0');
+      return initializeAppCheck(undefined, { provider, isTokenAutoRefreshEnabled: true });
     })
   ]
 };
